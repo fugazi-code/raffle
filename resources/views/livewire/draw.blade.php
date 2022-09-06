ef0ab69c9052dac590e6b79f8b2c9da39a1e4ce5
rtrenuela <rtrenuela@objectbright.com>
2022-09-06 10:17:44 +0800

    update

--- resources/views/livewire/draw.blade.php
+++ resources/views/livewire/draw.blade.php
@@ -80,110 +80,112 @@
             </div>
         </div>
     </div>
-        <script type="text/javascript">
+        @push('scripts')
+            <script type="text/javascript">
 
-            // Create new wheel object specifying the parameters at creation time.
-            var theWheel = new Winwheel({
-                'numSegments': {{ count($contestant) }},     // Specify number of segments.
-                'outerRadius': 212,   // Set outer radius so wheel fits inside the background.
-                'textFontSize': 20,    // Set font size as desired.
-                'segments':        // Define segments including colour and text.
-                    [
-                            @foreach($contestant as $key => $value)
+                // Create new wheel object specifying the parameters at creation time.
+                var theWheel = new Winwheel({
+                    'numSegments': {{ count($contestant) }},     // Specify number of segments.
+                    'outerRadius': 212,   // Set outer radius so wheel fits inside the background.
+                    'textFontSize': 20,    // Set font size as desired.
+                    'segments':        // Define segments including colour and text.
+                        [
+                                @foreach($contestant as $key => $value)
+                            {
+                                'fillStyle': @if($key%2) '#F7B80E' @else '#FCE32D' @endif,
+                                'id': '{{ $value->id }}', 'text': '#{{ $value->slot_no }} {{ $value->code_name }}'
+                            },
+                            @endforeach
+                        ],
+                    'animation':           // Specify the animation to use.
                         {
-                            'fillStyle': @if($key%2) '#F7B80E' @else '#FCE32D' @endif,
-                            'id': '{{ $value->id }}', 'text': '#{{ $value->slot_no }} {{ $value->code_name }}'
-                        },
-                        @endforeach
-                    ],
-                'animation':           // Specify the animation to use.
-                    {
-                        'type': 'spinToStop',
-                        'duration': 10, // Duration in seconds.
-                        'spins': 8, // Number of complete spins.
-                        'callbackFinished': alertPrize
+                            'type': 'spinToStop',
+                            'duration': 10, // Duration in seconds.
+                            'spins': 8, // Number of complete spins.
+                            'callbackFinished': alertPrize
+                        }
+                });
+
+                // Vars used by the code in this page to do power controls.
+                var wheelPower = 0;
+                var wheelSpinning = false;
+
+                // -------------------------------------------------------
+                // Function to handle the onClick on the power buttons.
+                // -------------------------------------------------------
+                function powerSelected(powerLevel) {
+                    // Ensure that power can't be changed while wheel is spinning.
+                    if (wheelSpinning == false) {
+                        // Reset all to grey incase this is not the first time the user has selected the power.
+                        document.getElementById('pw1').className = "";
+                        document.getElementById('pw2').className = "";
+                        document.getElementById('pw3').className = "";
+
+                        // Now light up all cells below-and-including the one selected by changing the class.
+                        if (powerLevel >= 1) {
+                            document.getElementById('pw1').className = "pw1";
+                        }
+
+                        if (powerLevel >= 2) {
+                            document.getElementById('pw2').className = "pw2";
+                        }
+
+                        if (powerLevel >= 3) {
+                            document.getElementById('pw3').className = "pw3";
+                        }
+
+                        // Set wheelPower var used when spin button is clicked.
+                        wheelPower = powerLevel;
+
+                        // Light up the spin button by changing it's source image and adding a clickable class to it.
+                        document.getElementById('spin_button').src = "spin_on.png";
+                        document.getElementById('spin_button').className = "clickable";
                     }
-            });
+                }
 
-            // Vars used by the code in this page to do power controls.
-            var wheelPower = 0;
-            var wheelSpinning = false;
+                // -------------------------------------------------------
+                // Click handler for spin button.
+                // -------------------------------------------------------
+                function startSpin() {
+                    theWheel.animation.spins = 15;
 
-            // -------------------------------------------------------
-            // Function to handle the onClick on the power buttons.
-            // -------------------------------------------------------
-            function powerSelected(powerLevel) {
-                // Ensure that power can't be changed while wheel is spinning.
-                if (wheelSpinning == false) {
-                    // Reset all to grey incase this is not the first time the user has selected the power.
-                    document.getElementById('pw1').className = "";
+                    // Begin the spin animation by calling startAnimation on the wheel object.
+                    theWheel.startAnimation();
+                }
+
+                // -------------------------------------------------------
+                // Function for reset button.
+                // -------------------------------------------------------
+                function resetWheel() {
+                    theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
+                    theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
+                    theWheel.draw();                // Call draw to render changes to the wheel.
+
+                    document.getElementById('pw1').className = "";  // Remove all colours from the power level indicators.
                     document.getElementById('pw2').className = "";
                     document.getElementById('pw3').className = "";
 
-                    // Now light up all cells below-and-including the one selected by changing the class.
-                    if (powerLevel >= 1) {
-                        document.getElementById('pw1').className = "pw1";
-                    }
-
-                    if (powerLevel >= 2) {
-                        document.getElementById('pw2').className = "pw2";
-                    }
-
-                    if (powerLevel >= 3) {
-                        document.getElementById('pw3').className = "pw3";
-                    }
-
-                    // Set wheelPower var used when spin button is clicked.
-                    wheelPower = powerLevel;
-
-                    // Light up the spin button by changing it's source image and adding a clickable class to it.
-                    document.getElementById('spin_button').src = "spin_on.png";
-                    document.getElementById('spin_button').className = "clickable";
+                    wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
                 }
-            }
 
-            // -------------------------------------------------------
-            // Click handler for spin button.
-            // -------------------------------------------------------
-            function startSpin() {
-                theWheel.animation.spins = 15;
+                // -------------------------------------------------------
+                // Called when the spin animation has finished by the callback feature of the wheel because I specified callback in the parameters
+                // note the indicated segment is passed in as a parmeter as 99% of the time you will want to know this to inform the user of their prize.
+                // -------------------------------------------------------
+                function alertPrize(indicatedSegment) {
+                    document.getElementById("winner-label").innerHTML += indicatedSegment.text;
+                    var myModal = new bootstrap.Modal(
+                        document.getElementById('winnerModal'), {
+                            keyboard: false
+                        })
+                    myModal.show();
+                    @this.setDrawed(indicatedSegment.id);
+                }
 
-                // Begin the spin animation by calling startAnimation on the wheel object.
-                theWheel.startAnimation();
-            }
-
-            // -------------------------------------------------------
-            // Function for reset button.
-            // -------------------------------------------------------
-            function resetWheel() {
-                theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
-                theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
-                theWheel.draw();                // Call draw to render changes to the wheel.
-
-                document.getElementById('pw1').className = "";  // Remove all colours from the power level indicators.
-                document.getElementById('pw2').className = "";
-                document.getElementById('pw3').className = "";
-
-                wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
-            }
-
-            // -------------------------------------------------------
-            // Called when the spin animation has finished by the callback feature of the wheel because I specified callback in the parameters
-            // note the indicated segment is passed in as a parmeter as 99% of the time you will want to know this to inform the user of their prize.
-            // -------------------------------------------------------
-            function alertPrize(indicatedSegment) {
-                document.getElementById("winner-label").innerHTML += indicatedSegment.text;
-                var myModal = new bootstrap.Modal(
-                    document.getElementById('winnerModal'), {
-                    keyboard: false
-                })
-                myModal.show();
-                @this.setDrawed(indicatedSegment.id);
-            }
-
-            function reloadPage()
-            {
-                location.reload();
-            }
-        </script>
+                function reloadPage()
+                {
+                    location.reload();
+                }
+            </script>
+        @endpush
 </div>

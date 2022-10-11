6b5201e41d651996350357673dc129ab7a125251
John Reinier <johnreiniervoluntad@gmail.com>
2022-10-11 14:42:04 +0800

    Added footer

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -36,6 +36,9 @@
     </div>
 </nav>
 @yield('content')
+
+
+
 <!-- Core JS -->
 <!-- build:js assets/vendor/js/core.js -->
 <script src="/theme/assets/vendor/libs/jquery/jquery.js"></script>
@@ -54,3 +57,46 @@
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 </html>
+
+<br>
+  <!-- Site footer -->
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            
+          <h1>About</h1>
+            <p class="text-justify">A lottery application. Thank you.<br> Thank you <br> 감사합니다 </p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Useful Links</h6>
+            <ul class="footer-links">
+              <li><a href="https://laravel.com/docs/9.x">Laravel</a></li>
+              <li><a href="https://laravel.com/docs/9.x/valet">Laravel Valet</a></li>
+              <li><a href="https://github.com/fzaninotto/Faker">Faker Factory Library</a></li>
+              <li><a href="https://www.w3schools.com/php/">PHP</a></li>
+            </ul>
+          </div>
+        
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="https://github.com/throwexceptions">About Us</a></li>
+              <li><a href="https://github.com/throwexceptions">Contact Us</a></li>
+              <li><a href="https://github.com/throwexceptions/raffle/pulls">Contribute</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
+         <a href="https://github.com/throwexceptions">ThrowExceptions</a>
+            </p>
+          </div>
+        </div>
+      </div>
+</footer>
